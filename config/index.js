0a2502a7442c11b52712a26af5c210e9942bca77
Nikolas <nihe@posteo.de>
2023-05-18 17:52:34 +0200

    change

--- config/index.js
+++ config/index.js
@@ -24,7 +24,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: ["http://localhost:3000", process.env.ORIGIN],
+      origin: process.env.ORIGIN,
     })
   );
   
--- server.js
+++ server.js
@@ -6,5 +6,3 @@ const PORT = process.env.PORT || 5005;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
-
-/// git
